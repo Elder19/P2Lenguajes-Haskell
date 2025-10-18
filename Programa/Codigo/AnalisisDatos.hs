358562b84b96688973bd5cd6370b01ff224df7f6
Elder19 <colegioempresarial20@gmail.com>
2025-10-18 01:18:20 -0600

    intento

--- Programa/Codigo/AnalisisDatos.hs
+++ Programa/Codigo/AnalisisDatos.hs
@@ -14,6 +14,7 @@ import Data.Time.Calendar (toGregorian, Day)
 import Text.Printf (printf)
 
 -- | Pausa breve para la interfaz de consola.
+
 pause :: IO ()
 pause = do
   putStrLn "Presione ENTER para continuar..."
